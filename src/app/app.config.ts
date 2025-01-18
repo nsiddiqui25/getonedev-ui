185afd6b1c39bcd252874537f8ced36373cd621b
Nabeel <n.siddiqui25@gmail.com>
2025-01-18 02:54:09 -0600

    new changes

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routeConfig } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routeConfig)]
-};
